57ea9cc9d84a2a4fe4a395aef3b88492bf5dded2
GenyaElamkov <GenyaElamkov@gmail.com>
2022-10-30 18:37:26 +0300

    Initial commit

---
+++ gui.py
@@ -0,0 +1,118 @@
+import tkinter as tk
+from datetime import datetime
+from tkinter import Label, Button, Entry, IntVar, StringVar
+from tkinter.messagebox import askyesno, showinfo
+from tkinter.ttk import Combobox
+
+from mail import send_email
+
+months = ['январь', 'февраль', 'март', 'апрель', 'май', 'июнь',
+          'июль', 'август', 'сентябрь', 'октябрь', 'ноябрь', 'декабрь']
+
+month = months[datetime.today().month - 1]
+year = datetime.today().year
+
+
+class Example(tk.Tk):
+
+    def __init__(self):
+        super().__init__()
+        self.center_window()
+
+        self.title('Показания электрического счетчика')
+        Label(self, text='Введите показания:', font=('Arial', 24, 'bold')).grid(row=0, column=0,
+                                                                                columnspan=4,
+                                                                                sticky='we')
+
+        Label(self, text='Месяц:', font=('Arial', 18, 'bold')).grid(row=1, column=0,
+                                                                    padx=10, pady=10)
+        self.var_month = StringVar()
+        self.var_month.set(month)
+        Combobox(self, values=months, textvariable=self.var_month, state='readonly', font=('Arial', 16),
+                 width=10).grid(row=1, column=1)
+
+        Label(self, text='Год:', font=('Arial', 18, 'bold')).grid(row=1,
+                                                                  column=2, padx=5, pady=10)
+        self.var_year = IntVar()
+        self.var_year.set(year)
+        Entry(self, textvariable=self.var_year, justify='center', font=('Arial', 18),
+              width=10).grid(row=1, column=3)
+
+        Label(self, text='T1:', font=('Arial', 18, 'bold')).grid(row=2,
+                                                                 column=0, padx=10, pady=10,
+                                                                 sticky='we')
+        self.var_t1 = IntVar()
+        Entry(self, textvariable=self.var_t1, font=('Arial', 18), justify='center').grid(row=2,
+                                                                                         column=1,
+                                                                                         padx=10,
+                                                                                         pady=10,
+                                                                                         columnspan=3)
+
+        Label(self, text='T2:', font=('Arial', 18, 'bold')).grid(row=3,
+                                                                 column=0, padx=10, pady=10)
+        self.var_t2 = IntVar()
+        Entry(self, textvariable=self.var_t2, font=('Arial', 18), justify='center').grid(row=3,
+                                                                                         column=1,
+                                                                                         padx=10,
+                                                                                         pady=10,
+                                                                                         columnspan=3)
+
+        Label(self, text='T3:', font=('Arial', 18, 'bold')).grid(row=4, column=0, padx=10, pady=10)
+        self.var_t3 = IntVar()
+        Entry(self, textvariable=self.var_t3, font=('Arial', 18), justify='center').grid(row=4,
+                                                                                         column=1,
+                                                                                         padx=10,
+                                                                                         pady=10,
+                                                                                         columnspan=3)
+
+        Button(self, text='Отправить', command=self.on_click, font=('Arial', 24, 'bold')).grid(row=5,
+                                                                                               column=0,
+                                                                                               columnspan=4,
+                                                                                               padx=10, pady=5,
+                                                                                               sticky='we')
+
+    def center_window(self) -> None:
+        w = 460
+        h = 350
+
+        sw = self.winfo_screenwidth()
+        sh = self.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        self.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+    def displays_message_sent(self, text):
+        showinfo('Информация', text)
+        self.quit()
+
+    def displays_message(self, dict_data):
+        label = f'''
+                Месяц:  {dict_data['месяц']}
+                Год:    {dict_data['год']}
+                T1:     {dict_data['T1']}
+                T2:     {dict_data['T2']}
+                T3:     {dict_data['T3']}
+        '''
+        res = askyesno(title='Отправить показания?', message=label)
+        if res:
+            resoult = send_email(dict_data['месяц'],
+                                 dict_data['год'],
+                                 dict_data['T1'],
+                                 dict_data['T2'],
+                                 dict_data['T3'])
+
+            self.displays_message_sent(resoult)
+
+    def on_click(self):
+        dict_data = {'месяц': self.var_month.get(),
+                     'год': self.var_year.get(),
+                     'T1': self.var_t1.get(),
+                     'T2': self.var_t2.get(),
+                     'T3': self.var_t3.get()}
+        self.displays_message(dict_data)
+
+
+if __name__ == "__main__":
+    app = Example()
+    app.mainloop()
---
+++ mail.py
@@ -0,0 +1,69 @@
+# import imaplib
+# import time
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formataddr
+from config import *
+from string import Template
+
+
+def send_email(month: str, year: int, t1: int, t2: int, t3: int) -> str:
+    from_addr = FROM_MAIL           # Почта отправителя.
+    to_addrs = TO_MAIL              # Почта принимающая.
+    password = PASSWORD             # Пароль отправителя.
+    host_smtp = HOST_SMTP           # Хост для исходящий сообщений.
+    port = PORT                     # Порт для исходящих сообщений.
+    subject = SUBJECT               # Тема письма.
+
+    # Сохранение не работает.
+    # mail_host_out = MAIL_HOST_OUT   # Хост для входящий сообщений.
+    # port_out = PORT_OUT             # Порт для входящих сообщений.
+
+    s = smtplib.SMTP(host_smtp, port)
+    s.starttls()
+
+    try:
+        with open('template.html', encoding='utf-8') as file:
+            template = file.read()
+            set_code_template = Template(template).safe_substitute(month=month,
+                                                                   year=year,
+                                                                   t1=t1,
+                                                                   t2=t2,
+                                                                   t3=t3)
+    except IOError:
+        return "The template file doesn't found!"
+
+    try:
+        s.login(from_addr, password)
+        msg = MIMEText(set_code_template, 'html')
+        msg['From'] = formataddr((SENDER_NAME, from_addr))
+        msg['To'] = formataddr((RECIPIENT_NAME, to_addrs))
+        msg['Subject'] = subject
+        s.sendmail(from_addr, to_addrs, msg.as_string())
+
+        # Не работает, выводит ошибку с encoding
+        # text = msg.as_string()
+
+        # imap = imaplib.IMAP4_SSL(mail_host_out, port_out)
+        # imap.login(from_addr, password)
+        # imap.append('Входящие', 'Отправленные', imaplib.Time2Internaldate(time.time()),
+        #             text.encode('utf-8'))
+        # imap.logout()
+
+        return 'Сообщение отправлено успешно!'
+    except Exception as _ex:
+        return f"{_ex}\nПожалуйста, проверьте свой логин или пароль!"
+
+
+def main():
+    month = 'октябрь'
+    year = 2022
+    t1 = 4545
+    t2 = 3148
+    t3 = 10299
+    out = send_email(month, year, t1, t2, t3)
+    print(out)
+
+
+if __name__ == '__main__':
+    main()
